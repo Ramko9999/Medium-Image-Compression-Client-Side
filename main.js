23300b502478d2a8c5a40696effc315ba0977620
Ramko9999 <ramapitchala@gmail.com>
2021-02-28 22:36:36 -0600

    Revoked Object URLs

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const IMAGE_QUALITY = 0.5;
+
 function compressImage(image, scale, initalWidth, initalHeight){
     return new Promise((resolve, reject) => {
 
@@ -10,9 +12,8 @@ function compressImage(image, scale, initalWidth, initalHeight){
         ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
         
         ctx.canvas.toBlob((blob) => {
-            canvas.remove();
             resolve(blob);
-        }, "image/png"); 
+        }, "image/png", IMAGE_QUALITY); 
     }); 
 }
 
@@ -34,23 +35,32 @@ imageInput.addEventListener("change", async (ev) => {
         return;
     }
 
-    const inputPreview = document.getElementById("input-preview");
-
+    //preview the inputted image
+    const inputPreview = document.getElementById("input-preview"); 
     inputPreview.src = URL.createObjectURL(uploadedImage);
-    const {height, width} = await getImageDimensions(inputPreview); //get the dimensions of the input image
+    
+    //get the dimensions of the input image
+    const {height, width} = await getImageDimensions(inputPreview); 
 
-    const MAX_WIDTH = 200;
-    const MAX_HEIGHT = 200;
+    const MAX_WIDTH = 200; //if we resize by width, this is the max width of compressed image
+    const MAX_HEIGHT = 200; //if we resize by height, this is the max height of the compressed image
 
     const widthRatioBlob = await compressImage(inputPreview, MAX_WIDTH / width, width, height); 
     const heightRatioBlob = await compressImage(inputPreview, MAX_HEIGHT / height, width, height);
  
-    const compressedBlob = widthRatioBlob.size > heightRatioBlob.size ? heightRatioBlob : widthRatioBlob; //pick the smallest blob
+    //pick the smaller blob between both
+    const compressedBlob = widthRatioBlob.size > heightRatioBlob.size ? heightRatioBlob : widthRatioBlob;
     
-    const outputPreview = document.getElementById("output-preview"); // preview the compressed blob
+    // preview the compressed blob
+    const outputPreview = document.getElementById("output-preview");
     outputPreview.src = URL.createObjectURL(compressedBlob);
 
-    //in some cases, the initial uploaded image maybe smaller than our compressed result. If that is the case, reuse the uploaded image
+    /*in some cases, the initial uploaded image maybe smaller than our compressed result. 
+      if that is the case, reuse the uploaded image. */
+  
     const optimalBlob = compressedBlob.size < uploadedImage.size ? compressedBlob : uploadedImage; 
     console.log(`Inital Size: ${uploadedImage.size}. Compressed Size: ${optimalBlob.size}`);
+    
+    URL.revokeObjectURL(inputPreview);
+    URL.revokeObjectURL(outputPreview);
 });
